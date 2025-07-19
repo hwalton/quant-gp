6ef77cbb8a5e98549d014ff43d3cccdc41587308
hwalton <harveywalton.hw@gmail.com>
2025-07-19 19:40:24 +0100

    benchmark 35.12 seconds to run

--- 3-optimise_portfolio/config.py
+++ 3-optimise_portfolio/config.py
@@ -7,13 +7,13 @@ class Config:
     y_std_pkl: str = '../2-gp_fit/y_std.npy'
     price_csv: str = '../0-data/bitcoin_combined_weekly_data.csv'
     initial_wealth: float = 1000
-    preference_curve: str = ['step', 'coordinate_points', 'log_risk_averse', 'general_risk_level'][1]
+    preference_curve: str = ['step', 'coordinate_points', 'log_risk_averse', 'general_risk_level'][0]
     gamma: float = 5  # Only used if utility_function is 'crra'
     step_threshold: float = 1100
     step_steepness: float = 100.0
 
-    horizon_weeks: int = 8
-    rebalance_every: int = 8  # weeks
+    horizon_weeks: int = 12
+    rebalance_every: int = 4  # weeks
 
     optimisation_method = ['bayesian', 'bayesian_with_refinement'][1]
     
